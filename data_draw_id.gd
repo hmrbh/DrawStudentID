8088b973d8dd863880fccb23779b4031934ba647
hmrBH <1218271192@qq.com>
2024-09-30 22:32:13 +0800

    清理项目

--- data_draw_id.gd
+++ data_draw_id.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var max_id: int = 0
+@export var min_id: int = 0
+
 var database: SQLite
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +15,8 @@ func _ready() -> void:
 	var loded_id_range = database.select_rows("id_range", "", ["*"])[0]
 	$"SettingsButton/Window/TabBar/TabContainer/通用/Label/min".text =  str(loded_id_range["min"])
 	$"SettingsButton/Window/TabBar/TabContainer/通用/Label/max".text =  str(loded_id_range["max"])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	max_id = int(database.select_rows("id_range", "", ["max"])[0]["max"])
+	min_id = int(database.select_rows("id_range", "", ["min"])[0]["min"])
 
 func create_database() -> void:
 	database = SQLite.new()
@@ -34,6 +35,11 @@ func create_database() -> void:
 	database.create_table("id_range", table_data)
 	
 func _on_min_id_text_edit_changed(new_text: String) -> void:
+	# 如果输入了非数字值就不记录直接退出
+	if not new_text.is_valid_int() and not new_text.is_empty():
+		print("错误输入！")
+		show_settings_error_dialog("位于设置->通用->设置学号范围->学号最小值输入框", "请检查您是否在此输入了非整数值")
+		return
 	var new_min_id = int(new_text)
 	if str(database.select_rows("id_range", "", ["*"])) == "[]":
 		create_database()
@@ -42,8 +48,16 @@ func _on_min_id_text_edit_changed(new_text: String) -> void:
 		print("检测到键值为空，插入一些初始数据防止更新失败")
 	database.query("UPDATE id_range SET min = " + str(new_min_id) + ";")
 	print(database.select_rows("id_range", "", ["*"]))
+	min_id = int(database.select_rows("id_range", "", ["min"])[0]["min"])
+	print("学号范围最小值被更改为：", min_id)
 
 func _on_max_id_text_edit_changed(new_text: String) -> void:
+	# 如果输入了非数字值就不记录直接退出
+	if not new_text.is_valid_int() and not new_text.is_empty():
+		print("错误输入！")
+		show_settings_error_dialog("位于设置->通用->设置学号范围->学号最大值输入框", "请检查您是否在此输入了非整数值")
+		return
+	
 	var new_max_id = int(new_text)
 	if str(database.select_rows("id_range", "", ["*"])) == "[]":
 		create_database()
@@ -52,8 +66,10 @@ func _on_max_id_text_edit_changed(new_text: String) -> void:
 		print("检测到键值为空，插入一些初始数据防止更新失败")
 	database.query("UPDATE id_range SET max = " + str(new_max_id) + ";")
 	print(database.select_rows("id_range", "", ["*"]))
+	max_id = int(database.select_rows("id_range", "", ["max"])[0]["max"])
+	print("学号范围最大值被更改为：", max_id)
 
-
-func _on_min_id_text_edit_ready() -> void:
-	print(111)
 	
+func show_settings_error_dialog(location: String, tip_msg: String) -> void:
+	$SettingsButton/AcceptDialog.visible = true
+	$SettingsButton/AcceptDialog.dialog_text = "您在设置项中输入了错误的内容\n" + location + "\n" + tip_msg
